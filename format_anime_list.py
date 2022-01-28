d3246a900b98bcaf33324cf27ab109757f00bc94
Johnny Wei <jwfiredragon@gmail.com>
2022-01-27 18:25:18 -0800

    Updated anime list

--- format_anime_list.py
+++ format_anime_list.py
@@ -1,6 +1,6 @@
 # with open('anime_list.txt', encoding = 'utf-8') as anime_list:
 # 	animes = anime_list.readlines()
-# 	animes = [anime.replace('\n', '') for anime in animes]
+# 	animes = [anime.replace('\n', '').replace('"', "'") for anime in animes]
 # 	animes = '",\n"'.join(animes)
 # 	animes = 'var anime_list = [\n"' + animes + '"\n]'
 #
@@ -9,4 +9,4 @@
 
 # the above, compacted into two lines for shits and giggles
 with open('anime_list.txt', encoding = 'utf-8') as al, open('docs/anime_list.js', 'w', encoding = 'utf-8') as o:
-		o.write('var anime_list = [\n"' + '",\n"'.join([a.replace('\n', '') for a in al.readlines()]) + '"\n]')
+		o.write('var anime_list = [\n"' + '",\n"'.join([a.replace('\n', '').replace('"', "'") for a in al.readlines()]) + '"\n]')
