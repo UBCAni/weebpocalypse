ef6e61ab80dbdea03ba7d7739d1501fc1a291e09
Johnny Wei <jwfiredragon@gmail.com>
2022-09-15 23:49:17 -0700

    Pause bat after running

--- auto_update.bat
+++ auto_update.bat
@@ -1,5 +1,5 @@
 python update_list.py
 git add --all
-git commit -m "Automatic list update %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git commit -m "Automatic list update: %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
 git push
-exit
+pause
