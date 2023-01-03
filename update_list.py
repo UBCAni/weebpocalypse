51a736523f1e7a2db37e96b104f28339b3f95eed
Johnny Wei <jwfiredragon@gmail.com>
2023-01-02 16:05:40 -0800

    Also fetch English titles

--- update_list.py
+++ update_list.py
@@ -10,29 +10,32 @@ if __name__ == '__main__':
 
 	with open('token.json') as file:
 		data = json.load(file)
+		access_token = data['access_token']
 		refresh_token = data['refresh_token']
 
-	# then request a new access token and save it to file
-	url = 'https://myanimelist.net/v1/oauth2/token'
+	# to disable token refreshing while testing
+	if True:
+		# then request a new access token and save it to file
+		url = 'https://myanimelist.net/v1/oauth2/token'
 
-	response = requests.post(url, data = {
-		'client_id': client_id,
-		'client_secret': client_secret,
-		'grant_type': 'refresh_token',
-		'refresh_token': refresh_token
-	})
+		response = requests.post(url, data = {
+			'client_id': client_id,
+			'client_secret': client_secret,
+			'grant_type': 'refresh_token',
+			'refresh_token': refresh_token
+		})
 
-	response.raise_for_status()
-	token = response.json()
-	response.close()
+		response.raise_for_status()
+		token = response.json()
+		response.close()
 
-	with open('token.json', 'w') as file:
-		json.dump(token, file, indent = 4)
+		with open('token.json', 'w') as file:
+			json.dump(token, file, indent = 4)
 
-	access_token = token['access_token']
+		access_token = token['access_token']
 
 	# fetch top 500 most popular anime
-	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&limit=500'
+	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&limit=500&fields=alternative_titles'
 	response1 = requests.get(url, headers = {
 		'Authorization': f'Bearer {access_token}'
 	})
@@ -42,7 +45,7 @@ if __name__ == '__main__':
 	response1.close()
 
 	# fetch next 500 most popular anime for 1000 total
-	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&limit=500&offset=500'
+	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&limit=500&offset=500&fields=alternative_titles'
 	response2 = requests.get(url, headers = {
 		'Authorization': f'Bearer {access_token}'
 	})
@@ -52,7 +55,7 @@ if __name__ == '__main__':
 	response2.close()
 
 	# fetch top 200 highest rated anime
-	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=200'
+	url = 'https://api.myanimelist.net/v2/anime/ranking?ranking_type=all&limit=200&fields=alternative_titles'
 	response3 = requests.get(url, headers = {
 		'Authorization': f'Bearer {access_token}'
 	})
@@ -62,12 +65,18 @@ if __name__ == '__main__':
 	response3.close()
 
 	with open('docs/anime_list.js', 'w', encoding='utf-8') as file:
-		# merge all three sets of animes, handling special characters and removing duplicates
 		anime_list = []
 		animes = animes1['data'] + animes2['data'] + animes3['data']
 		for anime in animes:
-			anime_list.append(anime['node']['title'].replace("\"", "'"))
-		anime_list = list(set(anime_list))
+			# add the default and English title of each anime to the list
+			title = anime['node']['title']
+			title_en = anime['node']['alternative_titles']['en']
+			if title_en == title or title_en == "":
+				anime_list.append(title.replace("\"", "'"))
+			else:
+				anime_list.append((title + ' / ' + title_en).replace("\"", "'"))
+		# remove duplicate entries
+		anime_id_list = list(set(anime_list))
 
 		# format the list into a Javascript array and write that to the list file
 		anime_list_txt = 'var anime_list = [\n'
